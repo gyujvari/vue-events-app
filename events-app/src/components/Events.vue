5d1a0aa1873f137dba7d5699df2d0b06efb346cc
Gyorgy <ujvari.gyorgy@gmail.com>
2023-07-23 16:40:46 +0300

    connected to api

--- events-app/src/components/Events.vue
+++ events-app/src/components/Events.vue
@@ -1,12 +1,40 @@
 <template>
-  <div>events</div>
+  <div>
+    <pre>
+      {{ data }}
+    </pre>
+  </div>
 </template>
 
 <script>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
 export default {
   name: "EventsComponent",
   props: {
-    data: String,
+    events: String,
+  },
+
+  setup() {
+    const data = ref();
+
+    onMounted(() => {
+      getData();
+    });
+
+    const getData = async () => {
+      await axios
+        .get("https://simple-events-api.onrender.com/events")
+        .then((response) => {
+          data.value = response.data;
+          console.log(response, "response");
+        });
+    };
+
+    return {
+      data,
+    };
   },
 };
 </script>
