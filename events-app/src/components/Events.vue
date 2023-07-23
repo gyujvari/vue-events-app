fb9e364a2bf5da9995702adf2ac935c8407b0ef6
Gyorgy <ujvari.gyorgy@gmail.com>
2023-07-23 17:36:09 +0300

    select a random item

--- events-app/src/components/Events.vue
+++ events-app/src/components/Events.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
+    <button @click="randomize()">Show</button>
     <pre>
-      {{ events }}
+      {{ selectedItem }}
     </pre>
   </div>
 </template>
 
 <script>
-import { onMounted, computed } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { useStore } from "vuex";
 
 export default {
@@ -15,6 +16,8 @@ export default {
 
   setup() {
     const store = useStore();
+    const count = ref(0);
+    const selectedItem = ref();
 
     const events = computed(() => {
       return store.getters.getData;
@@ -28,8 +31,17 @@ export default {
       store.dispatch("getData");
     };
 
+    const randomize = () => {
+      count.value = Math.floor(Math.random() * 3);
+      selectedItem.value = events.value.filter(
+        (item) => item.index === count.value
+      );
+    };
+
     return {
       events,
+      randomize,
+      selectedItem,
     };
   },
 };
